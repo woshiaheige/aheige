90b2452a94c813ead6e5f3c96893a3c15d9a5a47
defined <defined@163.com>
2020-03-27 16:00:45 +0800

    pm

--- src/views/workbench/workbench.vue
+++ src/views/workbench/workbench.vue
@@ -1,17 +1,20 @@
 <template>
-  <div>
-    工作台
-  </div>
+  <a-card :bordered="false" class="index">
+    <a-layout>
+      <a-layout-header>Header</a-layout-header>
+      <a-layout>
+        <a-layout-sider>Sider</a-layout-sider>
+        <a-layout-content>Content</a-layout-content>
+      </a-layout>
+    </a-layout></a-card
+  >
 </template>
-
 <script>
 export default {
-  name: "",
   data() {
     return {};
   },
+  mounted() {},
   methods: {}
 };
 </script>
-
-<style scoped lang="scss"></style>
