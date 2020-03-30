5e54edc0af1fcb8898fd2989520ced9e6dd2904c
defined <defined@163.com>
2020-03-30 11:00:06 +0800

    工作台

--- src/views/workbench/workbench.vue
+++ src/views/workbench/workbench.vue
@@ -1,20 +1,62 @@
 <template>
-  <a-card :bordered="false" class="index">
+  <a-layout>
+    <a-layout-header>
+      <div class="header-body">
+        <div v-for="(item, index) of cardList" :key="index" class="Card-body">
+          <a-card
+            :class="[item.CardClass]"
+            :title="item.header"
+            :bordered="false"
+          >
+            <p>{{ item.content }}</p>
+          </a-card>
+        </div>
+      </div>
+    </a-layout-header>
     <a-layout>
-      <a-layout-header>Header</a-layout-header>
-      <a-layout>
-        <a-layout-sider>Sider</a-layout-sider>
-        <a-layout-content>Content</a-layout-content>
-      </a-layout>
-    </a-layout></a-card
-  >
+      <a-layout-sider width="35%">
+        <div class="calendar-body">
+          <a-card>
+            <a-calendar :fullscreen="false" @panelChange="onPanelChange"
+          /></a-card>
+        </div>
+        <div class="calendar-body">
+          <a-card title="负责的站点(20个)">
+            <p>务庄污水(污水)</p>
+            <p>务庄污水(污水)</p>
+            <p>务庄污水(污水)</p>
+            <p>务庄污水(污水)</p>
+            <p>务庄污水(污水)</p>
+            <p>务庄污水(污水)</p>
+          </a-card>
+        </div>
+      </a-layout-sider>
+      <a-layout-content width="100%">
+        <a-card title="待完成的任务"></a-card>
+      </a-layout-content>
+    </a-layout>
+  </a-layout>
 </template>
+
 <script>
+import "@/assets/style/workbench/workbench.less";
+
 export default {
   data() {
-    return {};
+    return {
+      cardList: [
+        { header: "待完成任务", content: 5, CardClass: "unfinished" },
+        { header: "今日计划任务", content: 3, CardClass: "today" },
+        { header: "所有完成任务", content: 120, CardClass: "all" },
+        { header: "待完成报告", content: 0, CardClass: "report" }
+      ]
+    };
   },
   mounted() {},
-  methods: {}
+  methods: {
+    onPanelChange(value, mode) {
+      console.log(value, mode);
+    }
+  }
 };
 </script>
