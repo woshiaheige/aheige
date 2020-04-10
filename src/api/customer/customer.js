182153d64234e90f5eaa4dbeba24c0ac9d9073d8
foto <308816671@qq.com>
2020-04-10 11:37:33 +0800

    改回mock

--- src/api/customer/customer.js
+++ src/api/customer/customer.js
@@ -3,22 +3,29 @@ import axios from "../axios";
 
 const customer = {
   //获取一企一档列表
-  getEnterPriseList(data) {
-    return axios.get(base.api + "cusEnterprise/", {
-      params: data
-    });
+  // getEnterPriseList(data) {
+  //   return axios.get(base.mock + "cusEnterprise/", {
+  //     params: data
+  //   });
+  // },
+  getEnterPriseList() {
+    return axios.get(base.mock + "customer/enterprise-list");
   },
+
   //获取站点列表
   getStationList(data) {
-    return axios.get(base.api + "customer/station-list", {
+    return axios.get(base.mock + "customer/station-list", {
       params: data
     });
   },
   //获取合同列表
-  cusContract(data) {
-    return axios.get(base.api + "/cusContract/", {
-      params: data
-    });
+  // cusContract(data) {
+  //   return axios.get(base.mock + "/cusContract/", {
+  //     params: data
+  //   });
+  // }
+  cusContract() {
+    return axios.get(base.mock + "customer/contract-list");
   }
 };
 
--- src/views/customer/contract.vue
+++ src/views/customer/contract.vue
@@ -133,7 +133,7 @@ export default {
     }
   },
   mounted() {
-    // this.getTableData();
+    this.getTableData();
   }
 };
 </script>
