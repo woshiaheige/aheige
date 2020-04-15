0ec7b29678622108172bd8c74cebdc4e9b03b48a
foto <308816671@qq.com>
2020-04-15 10:17:59 +0800

    1

--- src/api/base.js
+++ src/api/base.js
@@ -7,9 +7,10 @@ const base = {
   file: ""
 };
 if (process.env.NODE_ENV == "development") {
-  base.api = "http://192.168.1.180:3102/api/";
+  base.api = "http://192.168.1.41:8102/api/";
+  // base.api = "http://192.168.1.180:3102/api/";
 } else if (process.env.NODE_ENV == "test") {
-  base.api = "http://192.168.10.101:3102/api/";
+  base.api = "http://192.168.10.101:3102/api";
 }
 
 export default base;
