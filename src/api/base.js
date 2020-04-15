2360fb5eeb8ea3e95a958084bb10a18430be89e0
foto <308816671@qq.com>
2020-04-15 16:33:54 +0800

    更正base.api

--- src/api/base.js
+++ src/api/base.js
@@ -7,10 +7,10 @@ const base = {
   file: ""
 };
 if (process.env.NODE_ENV == "development") {
-  // base.api = "http://192.168.1.180:3102/api/";
+  base.api = "http://192.168.1.180:3102/api/";
   // base.api = "http://192.168.1.69:3102/api/"; //有海
   // base.api = "http://192.168.10.101:3102/api/"; //101环境
-  base.api = "http://192.168.1.40:3102/api/"; //海宁
+  // base.api = "http://192.168.1.40:3102/api/"; //海宁
 } else if (process.env.NODE_ENV == "exam") {
   base.api = "http://192.168.10.101:3102/api/";
 }
