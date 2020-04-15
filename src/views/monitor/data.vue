5352ca6a683a755872b31cc5b06a08c297c460e7
zjc <823755316@qq.com>
2020-04-15 18:25:01 +0800

    修改监控中心页面

--- src/views/monitor/data.vue
+++ src/views/monitor/data.vue
@@ -1,11 +1,89 @@
 <template>
-  <a-card :bordered="false" class="standing" title="监测数据"> </a-card>
+  <a-card :bordered="false" class="standing" title="监测数据">
+    <a-layout>
+      <a-layout-sider><left-tree></left-tree></a-layout-sider>
+      <a-layout-content>
+        <a-form-model layout="inline" :model="formInline">
+          <a-form-model-item>
+            <a-range-picker @change="onChange" placeholder="时间范围" />
+          </a-form-model-item>
+          <a-form-model-item>
+            <a-button type="primary">
+              查询
+            </a-button>
+          </a-form-model-item>
+        </a-form-model>
+        <a-table
+          bordered
+          rowKey="id"
+          :columns="columns"
+          :dataSource="tableData"
+          v-margin:top="16"
+          :pagination="false"
+        >
+          <span slot="action" slot-scope="row">
+            <a @click="toMonitorData(row)">监测数据</a>
+          </span>
+        </a-table>
+
+        <a-pagination
+          v-margin:top="16"
+          showSizeChanger
+          :pageSize.sync="pageSize"
+          :defaultCurrent="current"
+          :total="total"
+        />
+      </a-layout-content>
+    </a-layout>
+  </a-card>
 </template>
 
 <script>
+import leftTree from "@/components/common/left-tree.vue";
 export default {
+  components: { leftTree },
   data() {
-    return {};
+    return {
+      pageSize: 10,
+      current: 1,
+      total: 0,
+      tableData: [
+        {
+          date: "2020-04-25 16:43:00",
+          temperature: "30",
+          gas: "二氧化碳"
+        },
+        {
+          date: "2020-04-25 16:43:00",
+          temperature: "30",
+          gas: "二氧化碳"
+        }
+      ],
+      columns: [
+        {
+          title: "时间",
+          dataIndex: "date",
+          key: "date",
+          align: "center"
+        },
+        {
+          title: "温度",
+          dataIndex: "temperature",
+          key: "temperature",
+          align: "center"
+        },
+        {
+          title: "废气",
+          dataIndex: "gas",
+          key: "gas",
+          align: "center"
+        }
+      ],
+      formInline: {
+        name: "",
+        type: ""
+      }
+    };
   },
   mounted() {},
   methods: {}
--- src/views/monitor/speed.vue
+++ src/views/monitor/speed.vue
@@ -1,11 +1,125 @@
 <template>
-  <div>
-    speed
-  </div>
+  <a-card :bordered="false" class="standing" title="传输速率">
+    <a-layout>
+      <a-layout-sider><left-tree></left-tree></a-layout-sider>
+      <a-layout-content>
+        <a-form-model layout="inline" :model="formInline">
+          <a-form-model-item>
+            <a-range-picker @change="onChange" placeholder="时间范围" />
+          </a-form-model-item>
+          <a-form-model-item>
+            <a-button type="primary">
+              查询
+            </a-button>
+          </a-form-model-item>
+        </a-form-model>
+        <a-table
+          bordered
+          rowKey="id"
+          :columns="columns"
+          :dataSource="tableData"
+          v-margin:top="16"
+          :pagination="false"
+        >
+          <span slot="action" slot-scope="row">
+            <a @click="toMonitorData(row)">监测数据</a>
+          </span>
+        </a-table>
+
+        <a-pagination
+          v-margin:top="16"
+          showSizeChanger
+          :pageSize.sync="pageSize"
+          :defaultCurrent="current"
+          :total="total"
+        />
+      </a-layout-content>
+    </a-layout>
+  </a-card>
 </template>
 
 <script>
-export default {};
+import leftTree from "@/components/common/left-tree.vue";
+export default {
+  components: { leftTree },
+  data() {
+    return {
+      pageSize: 10,
+      current: 1,
+      total: 0,
+      tableData: [
+        {
+          poiName: "中林木业-废气站房",
+          enterpriseName: "中林集团莆田国林木业城",
+          areaName: "莆田市",
+          hourRealCount: "30",
+          hourOughtCount: "15",
+          minRealCount: "65",
+          minOughtCount: "50",
+          dataTime: "2019-05-02~2020-05-01"
+        },
+        {
+          poiName: "豪华陶瓷-废气站房",
+          enterpriseName: "中林集团莆田国林木业城",
+          areaName: "莆田市",
+          hourRealCount: "29",
+          hourOughtCount: "14",
+          minRealCount: "70",
+          minOughtCount: "48",
+          dataTime: "2019-05-02~2020-05-01"
+        }
+      ],
+      columns: [
+        {
+          title: "时间",
+          dataIndex: "dataTime",
+          key: "dataTime",
+          align: "center"
+        },
+        {
+          title: "企业",
+          dataIndex: "enterpriseName",
+          key: "enterpriseName",
+          align: "center"
+        },
+        {
+          title: "监控点",
+          dataIndex: "poiName",
+          key: "poiName",
+          align: "center"
+        },
+        {
+          title: "分钟应收",
+          dataIndex: "minOughtCount",
+          key: "minOughtCount",
+          align: "center"
+        },
+        {
+          title: "分钟实收",
+          dataIndex: "minRealCount",
+          key: "minRealCount",
+          align: "center"
+        },
+        {
+          title: "小时应收",
+          dataIndex: "hourOughtCount",
+          key: "hourOughtCount",
+          align: "center"
+        },
+        {
+          title: "小时实收",
+          dataIndex: "hourRealCount",
+          key: "hourRealCount",
+          align: "center"
+        }
+      ],
+      formInline: {
+        name: "",
+        type: ""
+      }
+    };
+  },
+  mounted() {},
+  methods: {}
+};
 </script>
-
-<style></style>
