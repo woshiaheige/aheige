2d099cedf8774bb13c63f3fb5b0d582fef25f2ca
JimmyLam2010 <jimmy201028@gmail.com>
2020-04-17 09:00:13 +0800

    页头更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -55,10 +55,17 @@
           <li>凌可佳</li>
         </ul>
       </a-layout-header>
-      <a-page-header
-        @back="() => $router.go(-1)"
-        :breadcrumb="{ props: { routes } }"
-      >
+      <a-page-header @back="() => $router.go(-1)">
+        <a-breadcrumb :routes="routes" @itemRender="itemRender">
+          <template slot="itemRender" slot-scope="{ route, params, routes }">
+            <span v-if="routes.indexOf(route) === routes.length - 1">
+              {{ route.breadcrumbName }}
+            </span>
+            <router-link v-else :to="route.path">
+              {{ route.breadcrumbName }}
+            </router-link>
+          </template>
+        </a-breadcrumb>
         <template slot="backIcon">
           <a-icon type="arrow-left" v-show="$route.meta.back" />
         </template>
@@ -335,6 +342,9 @@ export default {
           breadcrumbName: this.$route.meta.title
         });
       }
+    },
+    itemRender(object) {
+      console.log(object);
     }
   }
 };
