96f0f7a4420b384624efead337a1bb80ae7d906e
zjc <823755316@qq.com>
2020-04-17 09:49:50 +0800

    修改数据报表

--- src/views/monitor/report.vue
+++ src/views/monitor/report.vue
@@ -1,99 +1,118 @@
 <template>
-  <a-card :bordered="false" class="standing" title="数据报表">
-    <a-layout>
-      <a-layout-sider><left-tree></left-tree></a-layout-sider>
-      <a-layout-content>
-        <a-form-model layout="inline" :model="formInline">
-          <a-form-model-item>
-            <a-range-picker />
-          </a-form-model-item>
-          <a-form-model-item>
-            <a-button type="primary">
-              查询
-            </a-button>
-          </a-form-model-item>
-        </a-form-model>
-        <a-table
-          size="middle"
-          bordered
-          rowKey="id"
-          :columns="columns"
-          :dataSource="tableData"
-          v-margin:top="16"
-          :pagination="false"
-        >
-          <span slot="action" slot-scope="row">
-            <a @click="toMonitorData(row)">监测数据</a>
-          </span>
-        </a-table>
+  <div>
+    <a-card :bordered="false" v-margin:bottom="28"
+      ><a-form-model layout="inline" :model="formInline">
+        <a-form-model-item>
+          <a-input v-model="formInline.enterpriseName" placeholder="企业名称" />
+        </a-form-model-item>
+        <a-form-model-item>
+          <a-input v-model="formInline.pointName" placeholder="监控点名称" />
+        </a-form-model-item>
+        <a-form-model-item>
+          <a-input v-model="formInline.mn" placeholder="MN号码" />
+        </a-form-model-item>
+        <a-form-model-item>
+          <a-button type="primary" @click="onSubmit">
+            查询
+          </a-button>
+        </a-form-model-item>
+      </a-form-model></a-card
+    >
+    <a-card :bordered="false" title="数据报表">
+      <a-table
+        size="middle"
+        bordered
+        rowKey="id"
+        :columns="columns"
+        :dataSource="tableData"
+        v-margin:top="16"
+        :pagination="false"
+      >
+        <span slot="action" slot-scope="row">
+          <a @click="toMonitorData(row)">监测数据</a>
+        </span>
+      </a-table>
 
-        <a-pagination
-          size="small"
-          v-margin:top="16"
-          showSizeChanger
-          :pageSize.sync="pageSize"
-          :defaultCurrent="current"
-          :total="total"
-        />
-      </a-layout-content>
-    </a-layout>
-  </a-card>
+      <a-pagination
+        size="small"
+        v-margin:top="16"
+        showSizeChanger
+        :pageSize.sync="pageSize"
+        :defaultCurrent="current"
+        :total="total"
+      />
+    </a-card>
+  </div>
 </template>
 
 <script>
-import leftTree from "@/components/common/left-tree.vue";
 export default {
-  components: { leftTree },
   data() {
     return {
       pageSize: 10,
       current: 1,
       total: 0,
-      tableData: [
-        {
-          id: "1",
-          date: "2020-04-14 00:00:00",
-          codstate: "17.2253",
-          anitrogen: "0.0191",
-          pHstate: "6"
-        },
-        {
-          id: "2",
-          date: "2020-04-14 00:00:00",
-          codstate: "17.2253",
-          anitrogen: "0.0191",
-          pHstate: "6"
-        }
-      ],
+      tableData: [],
       columns: [
         {
-          title: "时间",
-          dataIndex: "date",
-          key: "date",
+          title: "序号",
+          align: "center",
+          dataIndex: "index",
+          customRender: (text, row, index) => `${index + 1}`
+        },
+        {
+          title: "企业名称",
+          dataIndex: "enterpriseName",
+          key: "enterpriseName",
           align: "center"
         },
         {
-          title: "COD(mg/L)",
-          dataIndex: "codstate",
-          key: "codstate",
+          title: "监控点名称",
+          dataIndex: "pointName",
+          key: "pointName",
           align: "center"
         },
         {
-          title: "氨氮(mg/L)",
-          dataIndex: "anitrogen",
-          key: "anitrogen",
+          title: "分钟数据数",
+          dataIndex: "minDataCount",
+          key: "minDataCount",
           align: "center"
         },
         {
-          title: "PH",
-          dataIndex: "pHstate",
-          key: "pHstate",
+          title: "小时数据数",
+          dataIndex: "hourDataCount",
+          key: "hourDataCount",
+          align: "center"
+        },
+        {
+          title: "日数据数",
+          dataIndex: "dayDataCount",
+          key: "dayDataCount",
+          align: "center"
+        },
+        {
+          title: "应收条数",
+          dataIndex: "oughtCount",
+          key: "oughtCount",
+          align: "center"
+        },
+        {
+          title: "实收条数",
+          dataIndex: "realCount",
+          key: "realCount",
+          align: "center"
+        },
+        {
+          title: "实收条数",
+          dataIndex: "proCount",
+          key: "proCount",
           align: "center"
         }
       ],
       formInline: {
-        name: "",
-        type: ""
+        enterpriseName: "",
+        pointName: "",
+        mn: ""
       }
     };
   },
