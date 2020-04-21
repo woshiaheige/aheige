878b6307e9799e9ddb2314bd9c5a33e12256baeb
zjc <823755316@qq.com>
2020-04-21 15:08:19 +0800

    对接供应商接口

--- src/api/product/product.js
+++ src/api/product/product.js
@@ -2,13 +2,31 @@ import base from "../base";
 import axios from "../axios";
 
 const product = {
+  //获取供应商列表
   getSupplierList(data) {
     return axios.get(base.api + "supplier/", {
       params: data
     });
   },
+  //新增供应商
   postAddSupplier(data) {
     return axios.post(base.api + "supplier/addSupplier", data);
+  },
+  //删除供应商
+  delSupplier(data) {
+    return axios.get(base.api + "supplier/deleteSupplier", {
+      params: data
+    });
+  },
+  //修改供应商
+  postEditSupplier(data) {
+    return axios.post(base.api + "supplier/editSupplier", data);
+  },
+  //通过id查询供应商
+  getSupplierById(data) {
+    return axios.get(base.api + "supplier/getSupplierById", {
+      params: data
+    });
   },
   //获取试剂管理列表
   getGoodsList(data) {
