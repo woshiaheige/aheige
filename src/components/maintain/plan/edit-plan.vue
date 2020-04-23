b5f2dc6ec2511f4c9aefa11a6636fd6a6357947c
JimmyLam2010 <jimmy201028@gmail.com>
2020-04-23 09:40:00 +0800

    编辑计划

--- src/components/maintain/plan/edit-plan.vue
+++ src/components/maintain/plan/edit-plan.vue
@@ -15,14 +15,15 @@
       <a-form-model-item label="计划名称" prop="name">
         <a-input v-model="form.name" placeholder="请输入"></a-input>
       </a-form-model-item>
+      <a-form-model-item label="计划日期" prop="range">
+        <a-select v-model="form.date">
+          <a-select-option v-for="item in dateList" :key="item.value">{{
+            item.name
+          }}</a-select-option>
+        </a-select>
+      </a-form-model-item>
       <a-form-model-item label="运维期限" prop="range">
         <a-range-picker v-model="form.range"></a-range-picker>
-      </a-form-model-item>
-      <a-form-model-item label="运维小组" prop="group">
-        <a-select v-model="form.group">
-          <a-select-option :value="1">小组1</a-select-option>
-          <a-select-option :value="2">小组2</a-select-option>
-        </a-select>
       </a-form-model-item>
     </a-form-model>
   </a-modal>
@@ -44,17 +45,44 @@ export default {
     return {
       form: {
         name: "",
-        type: 1
+        date: 1
       },
       rules: {}
     };
   },
+  computed: {
+    dateList() {
+      if (this.planDetail.type == 1) {
+        return [
+          { name: "星期一", value: 1 },
+          { name: "星期二", value: 2 },
+          { name: "星期三", value: 3 },
+          { name: "星期四", value: 4 },
+          { name: "星期五", value: 5 },
+          { name: "星期六", value: 6 },
+          { name: "星期日", value: 0 }
+        ];
+      } else {
+        let arr = [];
+        for (let i = 1; i <= 31; i++) {
+          arr.push({
+            name: i + "号",
+            value: i
+          });
+        }
+        return arr;
+      }
+    }
+  },
   watch: {
-    planDetail(newVal) {
-      if (newVal.id) {
+    visible(newVal) {
+      if (newVal) {
         this.form = {
-          name: newVal.name,
-          range: [this.$moment(newVal.gmtBegin), this.$moment(newVal.gmtEnd)]
+          name: this.planDetail.name,
+          range: [
+            this.$moment(this.planDetail.gmtBegin),
+            this.$moment(this.planDetail.gmtEnd)
+          ]
         };
       }
     }
@@ -67,42 +95,19 @@ export default {
     handleOk() {
       this.$refs.form.validate(valid => {
         if (valid) {
-          if (this.schemeDetail.id) {
-            this.editScheme();
-          } else {
-            this.addScheme();
-          }
-        }
-      });
-    },
-    editScheme() {
-      let data = {
-        id: this.schemeDetail.id,
-        maintainType: this.maintainType,
-        name: this.form.name,
-        type: this.form.type
-      };
+          let data = {
+            id: this.schemeDetail.id,
+            maintainType: this.maintainType,
+            name: this.form.name,
+            type: this.form.type
+          };
 
-      this.$api.maintain.editScheme(data).then(res => {
-        if (res.data.state == 0) {
-          this.$message.success("编辑成功");
-          this.reset();
-          this.closeModal();
-        }
-      });
-    },
-    addScheme() {
-      let data = {
-        maintainType: this.maintainType,
-        name: this.form.name,
-        type: this.form.type
-      };
-
-      this.$api.maintain.addScheme(data).then(res => {
-        if (res.data.state == 0) {
-          this.$message.success("添加成功");
-          this.reset();
-          this.closeModal();
+          this.$api.maintain.editScheme(data).then(res => {
+            if (res.data.state == 0) {
+              this.$message.success("编辑成功");
+              this.closeModal();
+            }
+          });
         }
       });
     }
