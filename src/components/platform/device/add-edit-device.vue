b383570bab8c554b6a63d4ca9a6c394619c8378f
wulinfeng <jujing_yi@163.com>
2020-04-25 11:11:20 +0800

    解决

--- src/components/platform/device/add-edit-device.vue
+++ src/components/platform/device/add-edit-device.vue
@@ -16,13 +16,13 @@
       :wrapper-col="{ span: 18 }"
     >
       <a-form-model-item label="设备名称" prop="name">
-        <a-input placeholder="请输入" v-model.trim="formData.name" />
+        <a-input placeholder="请输入" v-model.trim="formData.name" @change="formDataName" />
       </a-form-model-item>
       <a-form-model-item label="生产厂家" prop="manufacturer">
-        <a-input placeholder="请输入" v-model.trim="formData.manufacturer" />
+        <a-input placeholder="请输入" v-model.trim="formData.manufacturer" @change="formDataManufacturer" />
       </a-form-model-item>
       <a-form-model-item label="设备型号" prop="number">
-        <a-input placeholder="请输入" v-model.trim="formData.number" />
+        <a-input placeholder="请输入" v-model.trim="formData.number" @change="formDataNumber" />
       </a-form-model-item>
       <a-form-model-item label="设备类型" prop="type">
         <a-select
@@ -30,11 +30,13 @@
           v-model="formData.type"
           showSearch
           :filterOption="filterOptions"
+          @change="searchDivisor"
         >
           <a-select-option
             v-for="item in pointOptions"
             :key="item.value"
             :value="Number(item.value)"
+            
           >
             {{ item.name }}
           </a-select-option>
@@ -46,7 +48,7 @@
           mode="multiple"
           v-model="formData.divisorIds"
           :filterOption="false"
-          @search="searchDivisor"
+          
           :notFoundContent="fetching ? undefined : null"
         >
           <a-spin v-if="fetching" slot="notFoundContent" size="small" />
@@ -129,19 +131,18 @@ export default {
   },
   mounted() {
     console.log(base.api);
-    this.searchDivisor();//丢wacth会导致出现变慢丢mounted生命周期好些
   },
   methods: {
     //因子下拉
     searchDivisor(value) {
-      console.log(111);
+      console.log(value);
       const fetchId = this.lastFetchId;
       this.data = [];
       this.fetching = true;
       let params = {
         size: 20,
         page: 1,
-        name: value
+        type: value
       };
       this.$api.platform.sysDivisor(params).then(res => {
         console.log(res)
@@ -154,6 +155,22 @@ export default {
           this.fetching = false;
         }
       });
+    },
+    //限制长度  自带的限制用户体验极差
+    formDataName(){
+      if(this.formData.name.length > 30){
+        this.formData.name = this.formData.name.substring(0,29)
+      }
+    },
+    formDataNumber(){
+      if(this.formData.number.length > 30){
+        this.formData.number = this.formData.number.substring(0,29)
+      }
+    },
+    formDataManufacturer(){
+      if(this.formData.manufacturer.length > 30){
+        this.formData.manufacturer = this.formData.manufacturer.substring(0,29)
+      }
     },
     handleOk() {
       this.$refs.ruleForm.validate(valid => {
