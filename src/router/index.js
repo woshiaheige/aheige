fc5475babcf772d02b66b2a61234f55bd8646f16
JimmyLam2010 <jimmy201028@gmail.com>
2020-04-26 14:57:40 +0800

    更新权限

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import client from "../assets/lib/checkBrowser";
-import routerTable from "./../router/routerTable";
+// import routerTable from "./../router/routerTable";
 
 Vue.use(VueRouter);
 
@@ -38,7 +38,7 @@ const router = new VueRouter({
   routes
 });
 
-router.addRoutes(routerTable);
+// router.addRoutes(routerTable);
 
 router.beforeEach(async (to, from, next) => {
   let token = sessionStorage.getItem("token");
