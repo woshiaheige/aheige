33d7af35b1d16f4f13ee26d03b359b590d78614e
JimmyLam2010 <jimmy201028@gmail.com>
2020-05-06 18:09:44 +0800

    更新页面

--- src/views/cost/device.vue
+++ src/views/cost/device.vue
@@ -44,7 +44,7 @@
     </a-card>
     <a-card :bordered="false" class="enterprise" v-margin:top="16">
       <div class="card-header">
-        <div class="title">设备列表</div>
+        <div class="title">出库详情</div>
       </div>
       <a-table
         rowKey="id"
@@ -55,21 +55,6 @@
         :pagination="false"
         :loading="loading"
       >
-        <template slot="controlLevel" slot-scope="controlLevel">
-          <a-tag color="red" v-if="controlLevel == 1">国控</a-tag>
-          <a-tag color="pink" v-if="controlLevel == 2">省控</a-tag>
-          <a-tag color="orange" v-if="controlLevel == 3">市控</a-tag>
-          <a-tag color="purple" v-if="controlLevel == 4">县控</a-tag>
-        </template>
-        <span slot="action" slot-scope="row">
-          <a @click="goPoint(row)">监控点管理</a>
-          <a-divider type="vertical" />
-          <a @click="goUser(row)">企业用户</a>
-          <a-divider type="vertical" />
-          <a @click="onEdit('edit', row)">编辑</a>
-          <a-divider type="vertical" />
-          <a @click="onDelete(row)">删除</a>
-        </span>
       </a-table>
 
       <a-pagination
@@ -83,41 +68,18 @@
         @change="pagechange"
         @showSizeChange="sizechange"
       />
-
-      <!-- 新建企业 -->
-      <add-enterprise
-        :controlOptions="controlOptions"
-        :typeList="typeList"
-        v-model="obj"
-        @refresh="getTableData"
-      />
-      <!-- 新建企业end -->
     </a-card>
   </div>
 </template>
 
 <script>
-import addEnterprise from "@/components/customer/enterprise/add-enterprise";
 export default {
-  name: "enterprise",
-  components: {
-    addEnterprise
-  },
   data() {
     return {
       current: 1,
       pageSize: 10,
       total: 0,
       loading: false,
-      controlOptions: [
-        { name: "国控", value: 1 },
-        { name: "省控", value: 2 },
-        { name: "市控", value: 3 },
-        { name: "县控", value: 4 }
-      ],
-      obj: {
-        show: false
-      },
       columns: [
         {
           title: "企业名称",
@@ -125,30 +87,39 @@ export default {
           key: "name"
         },
         {
-          title: "控制级别",
-          dataIndex: "controlLevel",
-          key: "controlLevel",
-          scopedSlots: { customRender: "controlLevel" },
-          align: "center",
-          width: 120
+          title: "监控点名称",
+          dataIndex: "name",
+          key: "name"
         },
         {
-          title: "行业类型",
-          dataIndex: "industryName",
-          key: "industryName",
-          width: 120
+          title: "品牌",
+          dataIndex: "name",
+          key: "name"
         },
         {
-          title: "环保负责人",
-          dataIndex: "environmentPrincipal",
-          key: "environmentPrincipal"
+          title: "型号",
+          dataIndex: "name",
+          key: "name"
         },
         {
-          title: "操作",
-          key: "action",
-          width: 250,
-          scopedSlots: { customRender: "action" },
-          align: "center"
+          title: "数量",
+          dataIndex: "name",
+          key: "name"
+        },
+        {
+          title: "单位",
+          dataIndex: "name",
+          key: "name"
+        },
+        {
+          title: "出库时间",
+          dataIndex: "name",
+          key: "name"
+        },
+        {
+          title: "总价",
+          dataIndex: "name",
+          key: "name"
         }
       ],
       tableData: [],
@@ -156,26 +127,12 @@ export default {
         name: "",
         level: "",
         type: ""
-      },
-      typeList: []
+      }
     };
   },
   mounted() {
     this.getTableData();
     this.getIndustrySelect();
-  },
-  beforeRouteLeave(to, from, next) {
-    if (
-      to.name === "customer-enterprise-station" ||
-      to.name === "customer-enterprise-user"
-    ) {
-      let arr = [];
-      arr.push("enterprise");
-      this.$store.dispatch("createIncludeArr", arr);
-    } else {
-      this.$store.dispatch("createIncludeArr", []);
-    }
-    next();
   },
   methods: {
     reset() {
@@ -211,51 +168,6 @@ export default {
           console.log(error);
           this.loading = false;
         });
-    },
-    //行业类型下拉
-    getIndustrySelect() {
-      this.$api.common.selectIndustry().then(res => {
-        this.typeList = res.data.data;
-      });
-    },
-    onDelete(row) {
-      let that = this;
-      this.$confirm({
-        title: "删除",
-        content: "是否删除",
-        onOk() {
-          that.$api.customer
-            .delEnterPrise({
-              id: row.id
-            })
-            .then(res => {
-              if (res.data.state == 0) {
-                that.$message.success("删除成功");
-                that.getTableData();
-              }
-            });
-        },
-        onCancel() {
-          console.log("Cancel");
-        }
-      });
-    },
-    goUser(row) {
-      this.$router.push({
-        path: "/customer/enterprise/user",
-        query: { id: row.id }
-      });
-    },
-    goPoint(row) {
-      this.$router.push({
-        path: "/customer/enterprise/station",
-        query: { id: row.id, enterpriseName: row.name }
-      });
-    },
-    onEdit(type, row) {
-      this.obj.show = true;
-      this.obj.type = type;
-      this.obj.row = row;
     }
   }
 };
