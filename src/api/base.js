62d583e88bdc8d0e087a3f8ace73692b86ea1058
foto <308816671@qq.com>
2020-05-09 09:36:59 +0800

    修复bug

--- src/api/base.js
+++ src/api/base.js
@@ -7,11 +7,7 @@ const base = {
   file: ""
 };
 if (process.env.NODE_ENV == "development") {
-<<<<<<< HEAD
-  base.api = "http://183.10.101.72:3102/api/"
-=======
-  base.api = "http://192.168.1.41:3102/api/";
->>>>>>> 2d77f9146ec922538d95973e35ddd9c87e9a8d97
+  base.api = "http://192.168.10.101:3102/api/";
 } else if (process.env.NODE_ENV == "exam") {
   base.api = "http://192.168.10.101:3102/api/";
 } else if (process.env.NODE_ENV == "production") {
