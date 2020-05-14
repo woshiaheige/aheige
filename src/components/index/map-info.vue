3f1915356074310c4fe7d86183d16842148cbccb
yangqiaomei <452051652@qq.com>
2020-05-14 17:33:53 +0800

    运维地图—监测点弹框修改

--- src/components/index/map-info.vue
+++ src/components/index/map-info.vue
@@ -1,30 +1,80 @@
 <template>
   <a-modal
     :title="title"
+    :style="style"
     :visible="modelData.show"
     @cancel="handleCancel"
     :mask="false"
     :footer="null"
+    class="map-modal"
   >
     <div v-margin:bottom="25">
-      <div class="content-window ant-row">
-        <div class="ant-col-12"><span>监控点名称：</span>{{ list.name }}</div>
-        <div class="ant-col-12">
-          <span>所属企业：</span>{{ list.enterpriseName }}
-        </div>
-        <div class="ant-col-12"><span>MN号：</span>{{ list.mn }}</div>
-        <div class="ant-col-12">
-          <span>监测因子数：</span>{{ list.divisorCount }}
-        </div>
-        <div class="ant-col-12">
-          <span>是否在线：</span>{{ list.state == 0 ? "在线" : "离线" }}
-        </div>
-        <div class="ant-col-12">
-          <span>是否异常：</span>{{ list.isNormal == 0 ? "正常" : "异常" }}
-        </div>
-        <div class="ant-col-24">
-          <span>最后通迅时间：</span>{{ list.communicationTime }}
-        </div>
+      <a-descriptions :column="2">
+        <a-descriptions-item label="监控点名称">
+          {{ list.name }}
+        </a-descriptions-item>
+        <a-descriptions-item label="所属企业">
+          {{ list.enterpriseName }}
+        </a-descriptions-item>
+        <a-descriptions-item label="MN号">
+          {{ list.mn }}
+        </a-descriptions-item>
+        <a-descriptions-item label="监测因子数">
+          {{ list.divisorCount }}
+        </a-descriptions-item>
+        <a-descriptions-item label="是否在线">
+          <a-tag color="green" v-if="list.state == 0">
+            在线
+          </a-tag>
+          <a-tag color="red" v-else>
+            离线
+          </a-tag>
+        </a-descriptions-item>
+        <a-descriptions-item label="是否异常">
+          <a-tag color="green" v-if="list.isNormal == 0">
+            正常
+          </a-tag>
+          <a-tag color="red" v-else>
+            异常
+          </a-tag>
+        </a-descriptions-item>
+        <a-descriptions-item label="是否超标">
+          <a-tag color="green" v-if="list.overFlag == 0">
+            正常
+          </a-tag>
+          <a-tag color="red" v-else>
+            超标
+          </a-tag>
+        </a-descriptions-item>
+        <a-descriptions-item label="最后通迅时间">
+          {{ list.communicationTime }}
+        </a-descriptions-item>
+      </a-descriptions>
+      <div v-if="!(JSON.stringify(realData) == '{}')">
+        <a-descriptions :title="'数据时间：' + realData.dataTime" :column="2">
+          <a-descriptions-item
+            v-for="(item, index) in realData.list"
+            :key="index"
+            :label="item.codeName"
+          >
+            <span v-if="item.Avg == '-'">{{ item.Avg }}</span>
+            <span v-else>
+              {{ item.Avg }}{{ item.unit }}
+              <a-tooltip placement="top">
+                <template slot="title">
+                  <p>上限：{{ item.ceilval }}</p>
+                  <p>下限：{{ item.floorval }}</p>
+                  <p>倍数：{{ item.multiple }}</p>
+                </template>
+                <a-icon
+                  type="question-circle"
+                  theme="filled"
+                  style="color: rgba(0, 0, 0, 0.25);"
+                />
+              </a-tooltip>
+            </span>
+          </a-descriptions-item>
+        </a-descriptions>
       </div>
     </div>
   </a-modal>
@@ -40,7 +90,9 @@ export default {
     return {
       loading: false,
       title: "",
-      list: {}
+      list: {},
+      realData: {},
+      style: ""
     };
   },
   computed: {
@@ -56,12 +108,56 @@ export default {
   mounted() {},
   watch: {
     "value.show"() {
+      this.list = {};
+      this.realData = {};
       if (this.value.show == true) {
         this.title = this.value.data.name;
         this.list = this.value.data;
+        this.style =
+          "top:" +
+          (this.value.position.y + 10) +
+          "px;left:" +
+          (this.value.position.x - 330) +
+          "px";
+        if (!(JSON.stringify(this.value.data.realData) == "{}")) {
+          this.realData = this.list.realData;
+          let num = Math.ceil(this.realData.list.length / 2) + 1;
+          this.style =
+            "top:" +
+            (this.value.position.y - num * 35) +
+            "px;left:" +
+            (this.value.position.x - 330) +
+            "px";
+        }
       }
     }
   }
 };
 </script>
-<style lang="less" scoped></style>
+<style lang="less">
+.map-modal {
+  .ant-modal-header {
+    background: #3792f7;
+    padding: 8px 20px;
+    font-size: 14px;
+    .ant-modal-title {
+      color: #fff;
+    }
+  }
+  .ant-modal-body {
+    padding-bottom: 10px !important;
+    & > div {
+      margin: 0 !important;
+    }
+  }
+  .ant-descriptions-title {
+    font-weight: initial;
+    font-size: 14px;
+  }
+  .ant-modal-close-x {
+    height: 42px;
+    line-height: 42px;
+    color: #fff;
+  }
+}
+</style>
