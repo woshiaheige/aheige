73647eb66437f0b61e10931b06b2caff25370a48
JimmyLam2010 <jimmy201028@gmail.com>
2020-05-16 15:57:42 +0800

    地址更新

--- src/api/base.js
+++ src/api/base.js
@@ -8,9 +8,8 @@ const base = {
 };
 if (process.env.NODE_ENV == "development") {
   base.api = "http://192.168.1.180:3102/api/";
-  // base.api = "http://192.168.10.104:3102/api/"; //演示环境
 } else if (process.env.NODE_ENV == "exam") {
-  base.api = "http://192.168.10.101:3102/api/";
+  base.api = "http://183.6.114.72:3102/api/";
 } else if (process.env.NODE_ENV == "production") {
   base.api = "http://183.6.114.72:3102/api/";
 }
