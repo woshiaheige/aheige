5cdae3ee242ec0400afaa3321d41264e496cdfe7
JimmyLam2010 <jimmy201028@gmail.com>
2020-05-25 17:41:32 +0800

    更新

--- src/views/maintain/dispatch.vue
+++ src/views/maintain/dispatch.vue
@@ -7,7 +7,12 @@
           <span>本日任务数：{{ this.data.length }}</span>
         </div>
       </div>
-      <a-radio-group v-model="week" v-margin:bottom="16" @change="changeWeek">
+      <a-radio-group
+        v-model="week"
+        v-margin:bottom="16"
+        @change="changeWeek"
+        buttonStyle="solid"
+      >
         <a-radio-button value="this">本周任务</a-radio-button>
         <a-radio-button value="next">下周任务</a-radio-button>
       </a-radio-group>
